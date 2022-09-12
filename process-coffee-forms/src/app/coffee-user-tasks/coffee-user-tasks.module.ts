6f912e23f09600cb7c0fb59d376795f54d819013
Torben Lottmann <torben.lottmann@lht.dlh.de>
2022-09-12 17:12:22 +0200

    added form to coffee user task

--- process-coffee-forms/src/app/coffee-user-tasks/coffee-user-tasks.module.ts
+++ process-coffee-forms/src/app/coffee-user-tasks/coffee-user-tasks.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReorderTaskComponent } from './reorder-task/reorder-task.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,9 @@ import { ReorderTaskComponent } from './reorder-task/reorder-task.component';
     ReorderTaskComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     ReorderTaskComponent
--- process-coffee-forms/src/app/coffee-user-tasks/reorder-task/reorder-task.component.ts
+++ process-coffee-forms/src/app/coffee-user-tasks/reorder-task/reorder-task.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reorder-task',
-  template: `
-    <p>
-      reorder-task works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './reorder-task.component.html',
+  styles: [`
+    .form-group > label {
+      font-weight: 500;
+    }
+  `]
 })
 export class ReorderTaskComponent implements OnInit {
 
-  constructor() { }
+  @Input() taskId!: string;
+  @Input() engineUrl!: string;
+  @Output() completed = new EventEmitter<string>();
+
+  form!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      beans: ['', Validators.required]
+    });
+  }
+
+  submitForm() {
+    console.log(`Submit form selection to backend and complete task`, this.form.getRawValue());
+    this.completed.emit(`Selected beans ${this.form.get('beans')?.value}`)
   }
 
 }
